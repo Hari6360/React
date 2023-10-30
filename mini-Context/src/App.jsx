7c4f78c86495728a0c578d94e9fc2caf39ce2719
Hari Prasad <101623030+Hari6360@users.noreply.github.com>
2023-10-30 16:45:53 +0530

    Learned new features and upadted context components

--- mini-Context/src/App.jsx
+++ mini-Context/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Navbar />
+			<Card />
+		</div>
+	);
 };
 
 export default App;
---
+++ themeContext/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+	<div className="flex flex-wrap min-h-screen items-center">
+		<div className="w-full">
+			<div className="w-full max-w-sm mx-auto flex justify-end mb-4">np</div>
+
+			<div className="w-full max-w-sm mx-auto"></div>
+		</div>
+	</div>;
+};
+
+export default App;
