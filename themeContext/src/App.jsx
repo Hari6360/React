dced88a514ba8b7ad70c1e1118e0741d7d3520c7
Hari Prasad <101623030+Hari6360@users.noreply.github.com>
2023-11-02 13:01:01 +0530

    Added toggle Button feature

--- themeContext/src/App.jsx
+++ themeContext/src/App.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { ThemeProvider } from "./Context/theme";
+import { useState } from "react";
+import ThemeBtn from "./Components/themeBtn";
+import Card from "./Components/card";
+import { useEffect } from "react";
 
 const App = () => {
-	<div className="flex flex-wrap min-h-screen items-center">
-		<div className="w-full">
-			<div className="w-full max-w-sm mx-auto flex justify-end mb-4">np</div>
+	const [themeMode, setThemeMode] = useState("light");
 
-			<div className="w-full max-w-sm mx-auto"></div>
-		</div>
-	</div>;
+	const lightThemeMode = () => {
+		setThemeMode("light");
+	};
+	const darkThemeMode = () => {
+		setThemeMode("dark");
+	};
+	useEffect(() => {
+		document.querySelector("html").classList.remove("light", "dark");
+		document.querySelector("html").classList.add(themeMode);
+	}, [themeMode]);
+
+	return (
+		<ThemeProvider value={{ themeMode, darkThemeMode, lightThemeMode }}>
+			<div className="flex flex-wrap min-h-screen items-center">
+				<div className="w-full">
+					<div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+						<ThemeBtn />
+					</div>
+
+					<div className="w-full max-w-sm mx-auto">
+						<Card />
+					</div>
+				</div>
+			</div>
+			;
+		</ThemeProvider>
+	);
 };
 
 export default App;
